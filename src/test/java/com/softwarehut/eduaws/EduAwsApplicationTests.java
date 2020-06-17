43b96ab98c91d7ccf8279ca294710d9f05b1ff1b
rkonopka <radoslaw.konopko@paxport.net>
2020-06-17 08:39:32 +0200

    Init commit

---
+++ src/test/java/com/softwarehut/eduaws/EduAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarehut.eduaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
